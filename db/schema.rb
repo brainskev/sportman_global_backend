bde46bd6ee0bea9b8d926c3ea6078a63118255fe
Kelvin <ngotho.kelvin@yahoo.com>
2023-12-04 21:12:28 +0300

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_04_175749) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "leagues", force: :cascade do |t|
+    t.string "name"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "matches", force: :cascade do |t|
+    t.string "team1"
+    t.string "team2"
+    t.integer "score_team1"
+    t.integer "score_team2"
+    t.datetime "date"
+    t.bigint "league_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["league_id"], name: "index_matches_on_league_id"
+  end
+
+  add_foreign_key "matches", "leagues"
 end
