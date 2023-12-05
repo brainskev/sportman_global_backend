c5a3477afd04ec12aaca9cf992e04c0050e0bffe
Kelvin <ngotho.kelvin@yahoo.com>
2023-12-05 23:30:18 +0300

    Add method to display matches

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,2 +1,7 @@
 class MatchesController < ApplicationController
+    berfore_action :set_match, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @matches = Matche.all 
+    end
 end
